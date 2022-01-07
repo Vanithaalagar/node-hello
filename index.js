c264ea400ed6045bca566373bc1968c220759e8a
Vanithaalagar <92861038+Vanithaalagar@users.noreply.github.com>
2022-01-07 11:25:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello nodejs!\n'
+  const msg = 'Hello vanitha!\n'
   res.end(msg);
 });
 
